1d15eb54e7c9cc276caf71a48761387c3b1fb8b0
Sonu Pandey <sonu.pandey@4moles.com>
2021-12-01 18:04:29 +0530

    receipt printing done

--- discount_calculator.rb
+++ discount_calculator.rb
@@ -1,26 +1,45 @@
-def user_inputs
-  puts "Please enter all the items purchased separated by a comma"
-  $items_bought = gets.chomp.split(',')
+require_relative './item.rb'
 
-  if $items_bought.include?('milk')
+
+# declaring variables for the storing the data
+$discounted_price = []
+$input_item = []
+$final_bill = []
+$total_actual_price = []
+
+def get_item
+  puts "Please enter all the items purchased separated by a comma"
+  $input_item = gets.chomp.split(',')
+
+  if $input_item.include?('milk')
     set_item("milk") 
   end
-  if $items_bought.include?('bread')
+  if $input_item.include?('bread')
    set_item( "bread")
   end 
-  if $items_bought.include?('banana') 
+  if $input_item.include?('banana') 
    set_item("banana")
   end 
 
-  if $items_bought.include?('apple') 
+  if $input_item.include?('apple') 
   set_item("apple")
   end
-end
 
+  
+  print_receipt
+end
 
 def set_item(item_name)
-  qty = $items_bought.count(item_name)
-  puts qty
-  
+  qty = $input_item.count(item_name)
+  item = Item.new(item_name, qty)
+  $final_bill << [item.item_name, item.item_quantity, "$#{item.item_discounted_price}"]
+  $discounted_price.push(item.item_discounted_price)
+  $total_actual_price.push(item.item_acutal_price)
 end
-user_inputs
+
+def print_receipt
+  amount_saved = ($total_actual_price.sum - $discounted_price.sum).round(2)
+  puts"\n Total price: $#{ $discounted_price.sum.round(2)} \n You saved $#{amount_saved} today."
+end
+
+get_item
---
+++ item.rb
@@ -0,0 +1,57 @@
+class Item
+  attr_accessor :item_name, :item_quantity, :item_discounted_price, :item_acutal_price
+  def initialize(item_name, item_quantity)
+    @items_details = {
+      "milk" => {  
+        "unit_price" => 3.97,
+        "sale_unit_price" => 2.50
+      },
+      "bread" => { 
+        "unit_price" => 2.17, 
+        "sale_unit_price" => 2.0, 
+      }, 
+      "apple" => { 
+        "unit_price" => 0.89
+      },"banana" =>{ 
+        "unit_price" => 0.99 
+      }
+    }
+    @item_name = item_name
+    @item_quantity = item_quantity
+    @item_discounted_price = calculate_item_discounted_price
+    @item_acutal_price = calculate_item_acutal_price  
+  end
+  
+  def calculate_item_discounted_price
+    case @item_name
+    when "milk"
+      sale_on_quantity = 2
+      final_milk_price = discount_calculator(@items_details[@item_name]["unit_price"], @items_details[@item_name]["sale_unit_price"],sale_on_quantity)
+      return final_milk_price 
+    when "bread"
+      sale_on_quantity = 3
+      final_bread_price = discount_calculator(@items_details[@item_name]["unit_price"], @items_details[@item_name]["sale_unit_price"],sale_on_quantity)
+      return final_bread_price
+    when "banana"
+      return @items_details[@item_name]["unit_price"] * @item_quantity
+    when "apple"
+      return @items_details[@item_name]["unit_price"] * @item_quantity
+    end
+  end
+
+  def calculate_item_acutal_price
+    return @items_details[@item_name]["unit_price"] * @item_quantity
+  end
+
+  def discount_calculator(item_unit_price, item_sale_unit_price, sale_on_quantity)
+    item_discounted_price = 0 
+    if @item_quantity < sale_on_quantity
+      item_discounted_price = item_unit_price * @item_quantity
+      return item_discounted_price
+    end
+    if @item_quantity >= sale_on_quantity
+      item_discounted_price = ((item_sale_unit_price * sale_on_quantity) * (@item_quantity / sale_on_quantity) ) + (item_unit_price * (@item_quantity % sale_on_quantity))
+      return item_discounted_price
+    end
+  end
+end
